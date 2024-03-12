cf4c9930d6cc25b04e885cb5da134c5589f0ef93
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-03-12 16:57:36 +0100

    add: app logo to the navbar

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className='w-20 h-full py-4 flex items-center justify-between flex-col border-2 border-gray-300 rounded-xl shadow-offset'>
+
             <div className='flex flex-col items-center gap-6'>
+                <img src="./images/zonta.png" alt="Zonta"></img>
                 <a className="text-4xl cursor-pointer text-gray-400 icon-[material-symbols--search-rounded]"></a>
+                <hr className='w-full border border-gray-400'></hr>
                 <a className="text-4xl cursor-pointer text-gray-400 icon-[material-symbols--article-outline]"></a>
                 <a className="text-4xl cursor-pointer text-gray-400 icon-[mdi--truck-outline]"></a>
                 <a className="text-4xl cursor-pointer text-gray-400 icon-[ic--outline-people]"></a>
