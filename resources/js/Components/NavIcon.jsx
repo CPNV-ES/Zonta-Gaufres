44fc752226059a6a695924045001077a821255f0
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-03-13 09:59:13 +0100

    add NavIcons to the Navbar

--- resources/js/Components/NavIcon.jsx
+++ resources/js/Components/NavIcon.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const NavIcon = ({icon, name, destination}) => {
+const NavIcon = ({ icon, name, destination }) => {
     return (
-        <a className="relative flex items-center text-lg overflow-hidden cursor-pointer" href={destination}>
-            <div>
-                <span className={`absolute text-4xl h-full text-center w-10 text-gray-400 icon-${icon}`}></span>
-                <span className='pl-12 block h-full align-middle'>{name}</span>
-            </div>
+        <a className="relative h-10 flex text-lg overflow-hidden cursor-pointer" href={destination}>
+            <span className={`absolute text-4xl h-full text-center w-10 text-gray-400 icon-${icon}`}></span>
+            <span className='pl-12 flex h-full text-xl items-center'>{name}</span>
         </a>
     )
 }
--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import NavIcon from './NavIcon'
 
 const Navbar = () => {
     return (
-        <nav className='w-20 h-full py-4 flex items-center justify-between flex-col border-2 border-gray-300 rounded-xl shadow-offset'>
+        <nav className='h-full p-4 flex flex-col items-center justify-between  border-2 border-gray-300 rounded-xl shadow-offset'>
+            <div className='w-10 flex flex-col gap-6'>
+                <img className='w-10' src="./images/zonta.png" alt="Zonta"></img>
+                <NavIcon name="Recherche" icon="[material-symbols--search-rounded]" />
 
-            <div className='flex flex-col items-center gap-6'>
-                <img src="./images/zonta.png" alt="Zonta"></img>
-                <a className="text-4xl cursor-pointer text-gray-400 icon-[material-symbols--search-rounded]"></a>
                 <hr className='w-full border border-gray-400'></hr>
-                <a className="text-4xl cursor-pointer text-gray-400 icon-[material-symbols--article-outline]"></a>
-                <a className="text-4xl cursor-pointer text-gray-400 icon-[mdi--truck-outline]"></a>
-                <a className="text-4xl cursor-pointer text-gray-400 icon-[ic--outline-people]"></a>
-                <a className="text-4xl cursor-pointer text-gray-400 icon-[gravity-ui--file-dollar]"></a>
+
+                <NavIcon name="Commandes" icon="[material-symbols--article-outline]" />
+                <NavIcon name="Livraisons" icon="[mdi--truck-outline]" />
+                <NavIcon name="Personnel" icon="[ic--outline-people]" />
+                <NavIcon name="Factures" icon="[gravity-ui--file-dollar]" />
             </div>
-            <a className="text-4xl cursor-pointer text-gray-400 icon-[mdi--cog]"></a>
+
+            <div className="w-10 flex flex-col gap-6">
+                <NavIcon name="Paramètres" icon="[mdi--cog]" />
+            </div>
         </nav>
     )
 }
