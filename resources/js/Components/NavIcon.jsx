4072a71b730a6a484a6fa2d1e40eb382dfd9268e
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-03-13 09:31:24 +0100

    add params to the NavIcon components

--- resources/js/Components/NavIcon.jsx
+++ resources/js/Components/NavIcon.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const NavIcon = () => {
+const NavIcon = ({icon, name, destination}) => {
     return (
-        <a className="relative flex items-center text-lg overflow-hidden  w-10" href="">
-            <span className="absolute text-4xl text-center w-10 text-gray-400 icon-[material-symbols--search-rounded]"></span>
-            <span className='pl-12 align-middle'>Recherche</span>
+        <a className="relative flex items-center text-lg overflow-hidden cursor-pointer" href={destination}>
+            <div>
+                <span className={`absolute text-4xl h-full text-center w-10 text-gray-400 icon-${icon}`}></span>
+                <span className='pl-12 block h-full align-middle'>{name}</span>
+            </div>
         </a>
     )
 }
