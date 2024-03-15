56a1ab863c85926e75e178ec0b864129d9ab60db
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-03-15 10:28:36 +0100

    fix: animation velocity/acceleration based on the NavIcons width

--- resources/js/Components/NavIcon.jsx
+++ resources/js/Components/NavIcon.jsx
@@ -4,7 +4,7 @@ const NavIcon = ({ icon, name, destination }) => {
     return (
         <a className="relative h-10 w-full flex text-lg overflow-hidden cursor-pointer rounded hover:bg-red-200" href={destination}>
             <span className={`absolute text-4xl h-full text-center w-10 text-gray-400 icon-${icon}`}></span>
-            <span className='pl-12 pr-2 flex h-full text-xl items-center'>{name}</span>
+            <span className='nav-label pl-12 pr-2 flex h-full text-xl items-center'>{name}</span>
         </a>
     )
 }
--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NavIcon from './NavIcon'
 
 const Navbar = () => {
 
     const [opened, setOpened] = useState(false)
+    const [maxNavWidth, setMaxNavWidth] = useState(40)
+
+    useEffect(() => {
+        if (!opened) return setMaxNavWidth(40)
+
+        // Get the width of the widest nav label
+        let navLabels = Array.from(document.getElementsByClassName('nav-label'))
+        navLabels = navLabels.map((label) => label.offsetWidth)
+        setMaxNavWidth(Math.max(...navLabels))
+    }, [opened])
 
     return (
-        <nav className='relative h-full p-4 flex flex-col items-center justify-between border-2 border-gray-300 rounded-xl shadow-offset'>
+        <nav className='relative h-full p-4 flex flex-col justify-between border-2 border-gray-300 rounded-xl shadow-offset'>
 
             <button 
             className="absolute translate-x-1/2 rounded-full right-0 h-6 w-6 flex justify-center items-center border-2 border-gray-300 shadow-offset bg-white hover:bg-gray-200"
@@ -15,7 +25,7 @@ const Navbar = () => {
                 <span className={`transition-all ease-in-out duration-500 rotate-${180*opened} icon-[mdi--chevron-right]`}></span>
             </button>
 
-            <div className={`transition-all ease-in-out duration-700 ${opened ? 'max-w-screen-sm' : 'max-w-10'} flex flex-col gap-6 overflow-hidden`}>
+            <div className='transition-all ease-in-out duration-700 flex flex-col items-start justify-start gap-6 overflow-hidden' style={{maxWidth: `${maxNavWidth}px`}}>
                 <img className='w-10' src="./images/zonta.png" alt="Zonta"></img>
                 <NavIcon name="Recherche" icon="[material-symbols--search-rounded]" />
 
@@ -27,7 +37,7 @@ const Navbar = () => {
                 <NavIcon name="Factures" icon="[gravity-ui--file-dollar]" />
             </div>
 
-            <div className={`transition-all ease-in-out duration-700 ${opened ? 'max-w-screen-sm' : 'max-w-10'} w-full flex flex-col gap-6 overflow-hidden`}>
+            <div className='transition-all ease-in-out duration-700 flex flex-col items-start justify-start gap-6 overflow-hidden' style={{maxWidth: `${maxNavWidth}px`}}>
                 <NavIcon name="Paramètres" icon="[mdi--cog]" />
             </div>
         </nav>
