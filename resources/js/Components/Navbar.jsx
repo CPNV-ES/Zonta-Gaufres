2901a96177841e891f4018cadb3e910f706a5d5c
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-03-15 09:11:12 +0100

    refactor: move the nav transformation from settings icon to a new nav button

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -6,7 +6,15 @@ const Navbar = () => {
     const [opened, setOpened] = useState(false)
 
     return (
-        <nav className='h-full p-4 flex flex-col items-center justify-between  border-2 border-gray-300 rounded-xl shadow-offset'>
+        <nav className='relative h-full p-4 flex flex-col items-center justify-between border-2 border-gray-300 rounded-xl shadow-offset'>
+
+            <button 
+            className="absolute translate-x-1/2 rounded-full right-0 h-6 w-6 flex justify-center items-center border-2 border-gray-300 shadow-offset bg-white hover:bg-gray-200"
+            onClick={() => setOpened(!opened)}
+            >
+                <span className={`transition-all ease-in-out duration-500 rotate-${180*opened} icon-[mdi--chevron-right]`}></span>
+            </button>
+
             <div className={`transition-all ease-in-out duration-700 ${opened ? 'max-w-screen-sm' : 'max-w-10'} flex flex-col gap-6 overflow-hidden`}>
                 <img className='w-10' src="./images/zonta.png" alt="Zonta"></img>
                 <NavIcon name="Recherche" icon="[material-symbols--search-rounded]" />
@@ -19,7 +27,7 @@ const Navbar = () => {
                 <NavIcon name="Factures" icon="[gravity-ui--file-dollar]" />
             </div>
 
-            <div onClick={() => setOpened(!opened)} className={`transition-all ease-in-out duration-700 ${opened ? 'max-w-screen-sm' : 'max-w-10'} w-full flex flex-col gap-6 overflow-hidden`}>
+            <div className={`transition-all ease-in-out duration-700 ${opened ? 'max-w-screen-sm' : 'max-w-10'} w-full flex flex-col gap-6 overflow-hidden`}>
                 <NavIcon name="Paramètres" icon="[mdi--cog]" />
             </div>
         </nav>
