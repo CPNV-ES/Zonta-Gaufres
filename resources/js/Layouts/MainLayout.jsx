1300f869d0ff61dd32dd7fa6015daf4f139e2c8b
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-03-18 11:20:24 +0100

    refactor: resize main layout to fill the whole window

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Navbar from '@/Components/Navbar'
 const MainLayout = ({ children }) => {
     return (
-        <>
+        <section id="main-layout">
             <Navbar />
-            <div className='h-full rounded-xl w-full border-2 border-gray-300 shadow-offset overflow-auto'>
+            <section className='h-full rounded-xl w-full border-2 border-gray-300 shadow-offset overflow-auto'>
                 {children}
-            </div>
-        </>
+            </section>
+        </section>
     )
 }
 
