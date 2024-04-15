d0d0986541cd0fa6605cc0c1bc2d78784d90f3e0
Benjamin Fontana <ben192003@gmail.com>
2024-04-15 15:18:52 +0200

    feat: add checkbox on selector & updated the columns for the new layout

--- resources/js/Pages/Invoices.jsx
+++ resources/js/Pages/Invoices.jsx
@@ -4,19 +4,37 @@ import DataTable from "@/Components/DataTable";
 
 import { ColumnBuilder } from "@/Builder/ColumnBuilder";
 
+import { Checkbox } from "@/Components/ui/checkbox";
+
 const builder = new ColumnBuilder();
 
 const columnHeaders = [
-    "selected",
-    "invoice_id",
-    "company",
-    "client",
-    "creation_date",
-    "payment_date",
-    "status",
-    "contact",
-    "total",
-    "actions",
+    {
+        name: "selected",
+        cell: ({ row }) => (
+            <div>
+                <Checkbox checked={row.original.selected} />
+            </div>
+        ),
+    },
+    { name: "invoice_id" },
+    { name: "company" },
+    { name: "client" },
+    { name: "creation_date" },
+    { name: "payment_date" },
+    { name: "status" },
+    { name: "contact" },
+    { name: "total" },
+    {
+        name: "actions",
+        cell: ({ row }) => (
+            <div>
+                <button>View</button>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        ),
+    },
 ];
 
 const columns = builder.buildColumns(columnHeaders);
@@ -46,7 +64,7 @@ const inputData = [
         contact: "Contact 2",
         total: "1000",
         actions: "Actions",
-    }
+    },
 ];
 
 const MainPage = () => {
