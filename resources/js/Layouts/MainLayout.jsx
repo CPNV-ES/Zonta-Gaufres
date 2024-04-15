2634aa4bf49a2acea73607a875172f380f09f75c
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-04-15 14:00:57 +0200

    fix : change background color for contrast

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -4,7 +4,7 @@ const MainLayout = ({ children, color='gray' }) => {
     return (
         <section id="main-layout" className={`bg-${color}-200`}>
             <Navbar />
-            <section className='h-full rounded-xl w-full border-2 border-gray-300 shadow-offset overflow-auto bg-white'>
+            <section className='h-full rounded-xl w-full border-2 border-gray-300 shadow-offset overflow-auto bg-gray-50'>
                 {children}
             </section>
         </section>
