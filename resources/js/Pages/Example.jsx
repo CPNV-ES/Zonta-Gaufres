c4359539b650ed9df4a43c0f5dd953412088a580
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-04-15 14:01:06 +0200

    feat : add exemple component

--- resources/js/Pages/Example.jsx
+++ resources/js/Pages/Example.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import MainLayout from '../Layouts/MainLayout'
+import DeliveryguysCard from "@/Components/DeliveryguysCard.jsx";
 
 const Example = () => {
   return (
     <MainLayout color='green'>
+        <DeliveryguysCard name={"Pierre-kiroul"} surname={"namassepasmousse"} city={"Lausanne"} orders={23} trips={3} />
+        <DeliveryguysCard name={"Paul"} surname={"Dupont"} city={"Yvonand"} orders={54} trips={5} />
+        <DeliveryguysCard name={"Paul"} city={"Yverdon"} orders={32} trips={7} />
+        <DeliveryguysCard name={"Paul"} city={"Les cluuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuds"} orders={2} trips={15} timetable={[{available: true},{available: false},{available: true},{available: false},{available: false},{available: true},{available: true},{available: false},{available: true},{available: false},{available: true},{available: true}]} />
+        <DeliveryguysCard name={"Paul"} city={"Les cluds"} orders={2} trips={15} timetable={[{available: true},{available: false},{available: true},{available: false},{available: false},{available: true},{available: true},{available: false},{available: true},{available: false},{available: true},{available: true}]} />
+
     </MainLayout>
   )
 }
