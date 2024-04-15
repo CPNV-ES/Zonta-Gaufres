fed4243573a7c701f04d7ad28a07968b148e8f2f
Benjamin Fontana <ben192003@gmail.com>
2024-04-15 15:17:27 +0200

    feat: accept more customization & error handling on required field

--- resources/js/Builder/ColumnBuilder.js
+++ resources/js/Builder/ColumnBuilder.js
@@ -6,15 +6,25 @@ export class ColumnBuilder {
         this.columns = [];
     }
 
-    buildColumns(columnsHeaders) {
-        columnsHeaders.forEach((columnHeader) => {
+    buildColumns(columnsDefinitions) {
+        this.checkRequiredValues(columnsDefinitions);
+        columnsDefinitions.forEach((colDef) => {
             this.columns.push(
-                this.columnHelper.accessor(columnHeader, {
-                    cell: (info) => info.getValue(),
-                    footer: (info) => info.column.id,
+                this.columnHelper.accessor(colDef.name, {
+                    id: colDef.id ?? null,
+                    cell: colDef.cell ?? ((info) => info.renderValue()),
+                    footer: colDef.footer ?? ((info) => info.column.id),
                 })
             );
         });
         return this.columns;
+    }
+
+    checkRequiredValues(columnsDefinitions) {
+        columnsDefinitions.forEach((colDef) => {
+            if (!colDef.name) {
+                throw new Error("Column name is required");
+            }
+        });
     }
 }
