5458602ee488b84fccd32452b1963567ea3b04c1
Benjamin Fontana <ben192003@gmail.com>
2024-04-26 14:59:36 +0200

    chore: add docstring for ColumnBuilder

--- resources/js/Builder/ColumnBuilder.js
+++ resources/js/Builder/ColumnBuilder.js
@@ -6,6 +6,20 @@ export class ColumnBuilder {
         this.columns = [];
     }
 
+    /**
+     * @typedef {Object} ColumnDefinition
+     * @property {string} accessor - The accessor of the column which will be used to get the data from the inputData object
+     * @property {string} header - The header of the column which will be displayed in the table
+     * @property {function} cell - The cell of the column which will be used to render the value of the column
+     * @property {function} footer - The footer of the column which will be displayed in the table
+     * @property {number} size - The size of the column which will be used to define the width of the column
+     */
+
+    /**
+     * Build columns for the DataTable component
+     * @param {[ColumnDefinition]} columnsDefinitions - Array of objects containing the column definitions
+     * @returns 
+     */
     buildColumns(columnsDefinitions) {
         this.checkRequiredValues(columnsDefinitions);
         columnsDefinitions.forEach((colDef) => {
