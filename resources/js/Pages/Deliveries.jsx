03de8febd716bc6f32e8e17ecc9d7060ef4e0a56
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-04-26 15:07:03 +0200

    feat: add page at /deliveries/edit

---
+++ resources/js/Pages/Deliveries.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import MainLayout from '../Layouts/MainLayout'
+import DeliveryguysCard from '@/Components/DeliveryguysCard.jsx'
+import DeliveryguysBigCard from '@/Components/DeliveryguysBigCard.jsx'
+import DeliveryItem from '@/Components/DeliveryItem.jsx'
+
+const Deliveries = () => {
+    const subject = 'Livraisons'
+    const color = 'blue'
+
+    return (
+        <MainLayout color={color} subject={subject}>
+          
+        </MainLayout>
+    )
+}
+
+export default Deliveries
