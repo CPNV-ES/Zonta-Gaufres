1c5eb312db3ff49376630697dac4a78f24f980f8
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-04-29 15:07:58 +0200

    feat: add Order model to include articles relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,7 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class, 'order_article')->withPivot('quantity');
+    }
 }
