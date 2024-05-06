d6c1371b53f94502740377ba75745c6717edcffd
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-05-06 15:39:29 +0200

    feat: add City model and its relationships

---
+++ app/Models/City.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class City extends Model
+{
+    public function address()
+    {
+        return $this->hasMany(Address::class);
+    }
+    public function deliverSchedule()
+    {
+        return $this->belongsToMany(DeliverSchedule::class);
+    }
+
+}
