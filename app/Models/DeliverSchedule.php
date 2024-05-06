daeb809aa84ecb9ca4224b3efed98585330dc77a
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-05-06 15:39:36 +0200

    feat: add DeliverSchedule model and its relationships

---
+++ app/Models/DeliverSchedule.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DeliverSchedule extends Model
+{
+    public function people()
+    {
+        return $this->belongsTo(People::class);
+    }
+    public function city()
+    {
+        return $this->belongsToMany(City::class);
+    }
+}
