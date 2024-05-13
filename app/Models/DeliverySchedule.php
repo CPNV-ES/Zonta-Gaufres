519e46265e75d2a41a3d7f08592aa06486077188
Benjamin Fontana <ben192003@gmail.com>
2024-05-13 15:42:45 +0200

    refactor: rename all deliver to delivery

--- app/Models/DeliverySchedule.php
+++ app/Models/DeliverySchedule.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DeliverSchedule extends Model
+class DeliverySchedule extends Model
 {
     public function person()
     {
