c9e22139e0ae6aa92ddf8cae29d5f6bdd73d9f2a
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-05-13 14:35:58 +0200

    refactor: Update foreign key names in Order model and related migrations

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -14,11 +14,11 @@ class Order extends Model
 
     public function buyer()
     {
-        return $this->belongsTo(People::class, 'buyer_id');
+        return $this->belongsTo(Person::class, 'buyer_id');
     }
     public function contact()
     {
-        return $this->belongsTo(People::class, 'contact_id');
+        return $this->belongsTo(Person::class, 'contact_id');
     }
     public function address()
     {
@@ -30,6 +30,6 @@ class Order extends Model
     }
     public function deliverSchedule()
     {
-        return $this->belongsTo(DeliverSchedule::class, 'deliver_schedule');
+        return $this->belongsTo(DeliverSchedule::class);
     }
 }
