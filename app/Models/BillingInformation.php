efbd4e91ffb7b85e7ec3680ee0d52d10e955fae3
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-05-13 14:48:11 +0200

    refactor: Rename 'billing_informations' table foreign key from 'people_id' to 'person_id'

--- app/Models/BillingInformation.php
+++ app/Models/BillingInformation.php
@@ -8,10 +8,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BillingInformation extends Model
 {
-    //TODO: remove after the BillingInfromations table is rename in migration
     protected $table = 'billing_informations';
-    public function people(): BelongsTo
+    public function person(): BelongsTo
     {
-        return $this->belongsTo(People::class);
+        return $this->belongsTo(Person::class);
     }
 }
