406999ae5023251559e09d061b689c201a8fb765
Benjamin Fontana <ben192003@gmail.com>
2024-05-13 14:30:54 +0200

    feat: use factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,15 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Address;
+use App\Models\AddressType;
+use App\Models\Article;
+use App\Models\BillingInformation;
+use App\Models\City;
+use App\Models\DeliverSchedule;
+use App\Models\Order;
+use App\Models\People;
+use App\Models\PeopleType;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +21,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        AddressType::factory(3)->create();
+        City::factory(50)->create();
+        Address::factory(10)->create();
+        Article::factory(1)->create();
+        BillingInformation::factory(1)->create();
+        DeliverSchedule::factory(20)->create();
+        PeopleType::factory(3)->create();
+        People::factory(100)->create();
+        Order::factory(100)->create();
     }
 }
