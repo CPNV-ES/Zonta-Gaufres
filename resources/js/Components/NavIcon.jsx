bdb3195542c012739cc89e37cd993440b74b005b
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-05-13 16:18:57 +0200

    feat: add search dialog on search icon

--- resources/js/Components/NavIcon.jsx
+++ resources/js/Components/NavIcon.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const NavIcon = ({ icon, name, destination, selected, color='gray' }) => {
+const NavIcon = ({icon, name, destination, selected, color = 'gray', onClick = null}) => {
 
     return (
-        <a className={`relative h-10 w-full flex text-lg overflow-hidden cursor-pointer rounded hover:bg-${color}-200`} href={destination}>
-            <span className={`absolute text-4xl h-full text-center w-10 icon-${icon} ${selected ? 'text-'+color+'-600' : 'text-gray-400'}`}>{name[0]}</span>
+        <a className={`relative h-10 w-full flex text-lg overflow-hidden cursor-pointer rounded hover:bg-${color}-200`}
+           href={destination} onClick={onClick}>
+            <span
+                className={`absolute text-4xl h-full text-center w-10 icon-${icon} ${selected ? 'text-' + color + '-600' : 'text-gray-400'}`}>{name[0]}</span>
             <span className='nav-label pl-12 pr-2 flex h-full text-xl items-center'>{name}</span>
         </a>
     )
--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, {useEffect, useState} from 'react'
 import NavIcon from './NavIcon'
+import Dialog from "@/Components/Dialog.jsx";
+import Search from "@/Components/Search.jsx";
 
-const Navbar = ({ color, subject }) => {
+const Navbar = ({color, subject}) => {
 
     const [opened, setOpened] = useState(false)
     const [maxNavWidth, setMaxNavWidth] = useState(40)
+    const [isDialogOpened, setIsDialogOpened] = useState(false)
 
     const NavIcons = [
-        { name: "Commandes", icon: "[material-symbols--article-outline]", color: 'green', destination: '/orders'},
-        { name: "Livraisons", icon: "[mdi--truck-outline]", color: 'blue', destination: '/deliveries' },
-        { name: "Personnel", icon: "[ic--outline-people]", color: 'yellow', destination: '/people' },
-        { name: "Factures", icon: "[gravity-ui--file-dollar]", color: 'red', destination: '/invoices'},
+        {name: "Commandes", icon: "[material-symbols--article-outline]", color: 'green', destination: '/orders'},
+        {name: "Livraisons", icon: "[mdi--truck-outline]", color: 'blue', destination: '/deliveries'},
+        {name: "Personnel", icon: "[ic--outline-people]", color: 'yellow', destination: '/people'},
+        {name: "Factures", icon: "[gravity-ui--file-dollar]", color: 'red', destination: '/invoices'},
     ]
 
     useEffect(() => {
@@ -23,30 +26,49 @@ const Navbar = ({ color, subject }) => {
     }, [opened])
 
     return (
-        <nav className={`main-layout-section relative p-4 flex flex-col justify-between border-${color}-400`}>
-
-            <button
-                className="absolute right-0 flex items-center justify-center w-6 h-6 translate-x-1/2 bg-white border-2 border-gray-300 rounded-full shadow-offset hover:bg-gray-200"
-                onClick={() => setOpened(!opened)}
+        <>
+            {/*children, title, description, buttonLabel, triggerLabel, setIsOpen, isOpen */}
+            <Dialog
+                title="Recherche"
+                description="Rechercher tout ce que vous voulez"
+                buttonLabel="rechercher"
+                isOpen={isDialogOpened}
+                setIsOpen={setIsDialogOpened}
             >
-                <span className='transition-all ease-in-out duration-700 icon-[mdi--chevron-right]' style={{ rotate: `${-180 * opened}deg` }}>&gt;</span>
-            </button>
+                <Search></Search>
+            </Dialog>
+            <nav className={`main-layout-section relative p-4 flex flex-col justify-between border-${color}-400`}>
 
-            <div className='flex flex-col items-start justify-start gap-6 overflow-hidden transition-all duration-700 ease-in-out' style={{ maxWidth: `${maxNavWidth}px` }}>
-                <img className='w-10' src="./images/zonta.png" alt="Zonta"></img>
-                <NavIcon name="Recherche" icon="[material-symbols--search-rounded]" />
-                <hr className='w-full rotate-[2] border border-gray-400'></hr>
+                <button
+                    className="absolute right-0 flex items-center justify-center w-6 h-6 translate-x-1/2 bg-white border-2 border-gray-300 rounded-full shadow-offset hover:bg-gray-200"
+                    onClick={() => setOpened(!opened)}
+                >
+                    <span className='transition-all ease-in-out duration-700 icon-[mdi--chevron-right]'
+                          style={{rotate: `${-180 * opened}deg`}}>&gt;</span>
+                </button>
 
-                {NavIcons.map((icon, index) => {
-                    return <NavIcon key={index} {...icon} selected={icon.name === subject} destination={icon.destination} />
-                })}
+                <div
+                    className='flex flex-col items-start justify-start gap-6 overflow-hidden transition-all duration-700 ease-in-out'
+                    style={{maxWidth: `${maxNavWidth}px`}}>
+                    <img className='w-10' src="./images/zonta.png" alt="Zonta"></img>
+                    <NavIcon name="Recherche" onClick={() => setIsDialogOpened(true)}
+                             icon="[material-symbols--search-rounded]"/>
+                    <hr className='w-full rotate-[2] border border-gray-400'></hr>
 
-            </div>
+                    {NavIcons.map((icon, index) => {
+                        return <NavIcon key={index} {...icon} selected={icon.name === subject}
+                                        destination={icon.destination}/>
+                    })}
 
-            <div className='flex flex-col items-start justify-start gap-6 overflow-hidden transition-all duration-700 ease-in-out' style={{ maxWidth: `${maxNavWidth}px` }}>
-                <NavIcon name="Paramètres" icon="[mdi--cog]" />
-            </div>
-        </nav>
+                </div>
+
+                <div
+                    className='flex flex-col items-start justify-start gap-6 overflow-hidden transition-all duration-700 ease-in-out'
+                    style={{maxWidth: `${maxNavWidth}px`}}>
+                    <NavIcon name="Paramètres" icon="[mdi--cog]"/>
+                </div>
+            </nav>
+        </>
     )
 }
 
