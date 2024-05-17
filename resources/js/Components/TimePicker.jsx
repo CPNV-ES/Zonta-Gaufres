b13fc5bc68e18779d4b28e520d343c6dd440c982
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-05-17 16:22:16 +0200

    feat: add timepicker component

---
+++ resources/js/Components/TimePicker.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+
+const TimePicker = () => {
+    const [time, setTime] = useState('18:00')
+    const [isTimeValid, setIsTimeValid] = useState(true)
+
+    const checkTime = (time) => {
+        setTime(time)
+        const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/
+        if (regex.test(time)) {
+            setIsTimeValid(true)
+        }
+        else {
+            setIsTimeValid(false)
+        }
+    }
+
+    return (
+        <div className="flex">
+            <input 
+            type="time" 
+            id="time" 
+            className={`rounded-lg bg-gray-50 border text-gray-900  block flex-1 w-full text-sm p-2.5 outline-none ${isTimeValid? 'border-gray-300' : 'border-red-500'}`}            value={time}
+            onChange={(e) => checkTime(e.target.value)}
+            required/>
+        </div>
+    )
+}
+
+export default TimePicker
