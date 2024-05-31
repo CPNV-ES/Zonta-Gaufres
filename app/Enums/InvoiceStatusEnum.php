6f1d530d12a7f2fd8d825b280bd76770a88f75c8
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-05-31 14:47:04 +0200

    feat: add InvoiceStatusEnum for invoice status values

---
+++ app/Enums/InvoiceStatusEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+enum InvoiceStatusEnum: string
+{
+    case PAID = 'Payée';
+    case OPEN = 'Ouverte';
+    case CANCELED = 'Annulée';
+    case INPROGRESS = 'En cours';
+    public static function fromCase(string $case): self
+    {
+        return constant("self::$case");
+    }
+}
