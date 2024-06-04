0a7730d7bca0c309ebd26333b4bdc4e522f1850b
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-06-04 16:03:30 +0200

    feat: add update method on orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DeliveryGuySchedule;
 use Inertia\Inertia;
 use App\Models\Order;
+use Illuminate\Http\Request;
+
 
 class OrderController extends Controller
 {
+    public function update(Request $request, string $id)
+    {
+        $order = Order::find($id);
+        $order->update($request->all());
+    }
 }
