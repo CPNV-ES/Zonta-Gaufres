33161a97ebdc0c832f8181b5f93a4a6ac6f3fcae
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-06-04 08:09:11 +0200

    feat: create Create order page

---
+++ resources/js/Pages/Orders/Create.jsx
@@ -0,0 +1,11 @@
+import CreateOrderForm from "@/Components/CreateOrderForm.jsx";
+import React from "react";
+
+const Create = (contactPeopleNames) => {
+    contactPeopleNames = contactPeopleNames.contactPeopleNames;
+    return (
+        <>
+        </>
+    )
+}
+export default Create;
