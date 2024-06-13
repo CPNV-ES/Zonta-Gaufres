7759ffbd25e2777381eb2a95e23e7bdbe1318930
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-06-13 16:42:23 +0200

    feat: use form component into create page

--- resources/js/Pages/Orders/Create.jsx
+++ resources/js/Pages/Orders/Create.jsx
@@ -1,11 +1,14 @@
 import CreateOrderForm from "@/Components/CreateOrderForm.jsx";
 import React from "react";
+import MainLayout from "../../Layouts/MainLayout";
 
 const Create = (contactPeopleNames) => {
     contactPeopleNames = contactPeopleNames.contactPeopleNames;
     return (
-        <>
-        </>
+        <MainLayout color="green" subject="Commandes">
+            <CreateOrderForm contactPeopleNames={contactPeopleNames}></CreateOrderForm>
+        </MainLayout>
+
     )
 }
 export default Create;
