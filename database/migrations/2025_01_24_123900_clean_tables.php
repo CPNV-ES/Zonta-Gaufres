c0e7d6522c5c53ae6c8a75b0a95591aa9da43ad8
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-01-30 12:11:12 +0100

    feat: add down feature for the migration

--- database/migrations/2025_01_24_123900_clean_tables.php
+++ database/migrations/2025_01_24_123900_clean_tables.php
@@ -11,8 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-
-
         Schema::dropIfExists('order_article');
         Schema::dropIfExists('articles');
         Schema::dropIfExists('billing_informations');
@@ -33,6 +31,49 @@ return new class extends Migration
     public function down(): void
     {
 
-        //
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_payment_type_id_foreign');
+            $table->dropColumn('payment_type_id');
+            $table->dropColumn('waffle_quantity');
+            $table->dropForeign('orders_address_id_foreign');
+            $table->dropColumn('address_id');
+        });
+
+        Schema::create('order_article', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained();
+            $table->foreignId('article_id')->constrained();
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->float('price');
+            $table->timestamps();
+        });
+
+        Schema::create('billing_informations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+        });
+
+        Schema::create('addresse_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('order_address', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained();
+            $table->foreignId('address_id')->constrained();
+            $table->foreignId('addresse_type_id')->constrained();
+            $table->timestamps();
+        });
     }
 };
