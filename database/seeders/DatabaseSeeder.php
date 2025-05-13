e8f01c78a42b992bb8d7550cdd59294da14a6bae
Ethann <ethann@shkermit.ch>
2025-05-13 12:12:02 +0200

    refactor: remove unused enums and clean up seeder code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,12 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Enums\InvoiceStatusEnum;
-use App\Enums\PaymentTypesEnum;
-use App\Enums\PersonTypesEnum;
 use Illuminate\Database\Seeder;
 use App\Models\Address;
 use App\Models\City;
-use App\Models\DeliveryGuySchedule;
-use App\Models\InvoiceStatus;
 use App\Models\Order;
-use App\Models\PaymentTypes;
 use App\Models\Person;
 use App\Models\PersonType;
-use App\Models\Invoice;
 
 class DatabaseSeeder extends Seeder
 {
@@ -29,9 +22,5 @@ class DatabaseSeeder extends Seeder
         Address::factory(10)->create();
         Person::factory(100)->create();
         Order::factory(100)->create();
-
-        foreach (Person::all() as $person) {
-            $person->personType()->attach(PersonType::all()->random());
-        }
     }
 }
