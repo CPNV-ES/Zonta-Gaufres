3fe2f0366d637fafb4e00519315f612ce233d50f
Ethann <ethann@shkermit.ch>
2025-06-05 11:57:39 +0200

    fix: clean up Navbar component by removing unnecessary NavIcon

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -63,11 +63,7 @@ const Navbar = ({ color, subject }) => {
                 className="flex flex-col items-start justify-start gap-6 overflow-hidden transition-all duration-700 ease-in-out"
                 style={{ maxWidth: `${maxNavWidth}px` }}
             >
-                <img className="w-10" src="/images/zonta.png" alt="Zonta"></img>
-                <NavIcon
-                    name="Recherche"
-                    icon="[material-symbols--search-rounded]"
-                />
+                <img className="w-10" src="/images/zonta.png" alt="Zonta" />
                 <hr className="w-full rotate-[2] border border-gray-400"></hr>
 
                 {NavIcons.map((icon, index) => {
